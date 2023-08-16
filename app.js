3f829717a3f9d34ea1fce63e2d8626e265b00661
Andrey Roditelev <rodandr13@gmail.com>
2023-08-16 16:57:08 +0300

    feat: add logger

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -11,12 +12,19 @@ const moviesRouter = require('./routes/movies');
 const { auth } = require('./middlewares/auth');
 const errorHandler = require('./middlewares/error-handler');
 const {NotFoundError} = require("./errors/NotFoundError");
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 const { PORT = 3000, MONGODB_URI = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
-
 mongoose.connect(MONGODB_URI);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+app.use(limiter);
+app.use(helmet());
 app.use(bodyParser.json());
+app.use(requestLogger);
 app.use('/signin', login);
 app.use('/signup', createUser);
 app.use('/users', auth, usersRouter);
@@ -24,6 +32,7 @@ app.use('/movies', auth, moviesRouter);
 app.use(auth, (req, res, next) => {
   next(new NotFoundError('Страница не найдена.'));
 });
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 app.listen(PORT);
